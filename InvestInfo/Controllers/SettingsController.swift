fc7fa2265678c0281c9aff96f7f6c246d7ade549
Vladimir Mikishchenko <v.mikishchenko@gmail.com>
2022-09-23 19:37:06 +0300

    добавил хранение настроек, смену аватара и имени

--- InvestInfo/Controllers/SettingsController.swift
+++ InvestInfo/Controllers/SettingsController.swift
@@ -9,19 +9,31 @@ import UIKit
 
 final class SettingsController: UITableViewController {
     private var vms: [CommonCellVM] = []
+    private var settingsDataSource: SettingsDataProtocol = SettingsDataSouce()
+    private lazy var imagePicker: UIImagePickerController = { UIImagePickerController() }()
+    private var newUserName: String?
+    private let cancelAction = UIAlertAction(title: "Отменить", style: .cancel)
+    private var isUserDetailsEditing = false {
+        didSet {
+            let item: UIBarButtonItem.SystemItem = isUserDetailsEditing ? .done : .edit
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: item, target: self, action: #selector(editButtonTap))
+        }
+    }
     
-    enum CellName: String, Equatable { case noOne, pushNotifications, createNews }
-    struct SettingsCellName: CommonCellNameProtocol { var name: CellName = .noOne }
-
+    private enum CellName: String, Equatable { case noOne, userDetails, pushNotifications, createNews }
+    private struct SettingsCellName: CommonCellNameProtocol { var name: CellName = .noOne }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
+        setupNavBar()
         tableView.separatorStyle = .none
-        tableView.register(SwitcherCell.self, forCellReuseIdentifier: "SwitcherCell")
+        tableView.register(SwitcherCell.self, forCellReuseIdentifier: String(describing: SwitcherCell.self))
+        tableView.register(UserDetailsCell.self, forCellReuseIdentifier: String(describing: UserDetailsCell.self))
         [ "SpaceCell" ].forEach {
             tableView.register(UINib(nibName: $0, bundle: nil), forCellReuseIdentifier: $0)
         }
-        createData()
+        updateData()
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -33,9 +45,9 @@ final class SettingsController: UITableViewController {
         cell.parentViewController = self
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return vms.count
+        vms.count
     }
 }
 
@@ -43,22 +55,144 @@ final class SettingsController: UITableViewController {
 extension SettingsController: SwitcherProtocol {
     func switcherChange(state: Bool, name: CommonCellNameProtocol) {
         guard let localName = (name as? SettingsCellName)?.name else { return }
-        //TODO: Надо менять состояния свитчеров в модели
         switch localName {
-        case .pushNotifications: break
-        case .createNews: break
+        case .pushNotifications:    settingsDataSource.setValue(state, for: .pushNotifications)
+        case .createNews:           settingsDataSource.setValue(state, for: .createNews)
         default: break
         }
     }
 }
 
+// MARK: - UserDetailsEditingProtocol
+extension SettingsController: UserDetailsEditingProtocol {
+    func editAvatar() {
+        guard isUserDetailsEditing else { return }
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            alert.addAction(UIAlertAction(title: "Сделать фото", style: .default) { [weak self] _ in
+                self?.showImagePicker(sourceType: .camera)
+            })
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            alert.addAction(UIAlertAction(title: "Выбрать из галереи", style: .default) { [weak self] _ in
+                self?.showImagePicker(sourceType: .photoLibrary)
+            })
+        }
+        if settingsDataSource.getValue(.avatarAvailable) {
+            alert.addAction(UIAlertAction(title: "Удалить", style: .destructive) { [weak self] _ in
+                self?.showRemovingPhotoAlert()
+            })
+        }
+        if !alert.actions.isEmpty {
+            alert.addAction(cancelAction)
+        }
+        present(alert, animated: true)
+    }
+    
+    func editUserName() {
+        guard isUserDetailsEditing else { return }
+        let alert = UIAlertController(title: "Имя пользователя",
+                                      message: "поможет в общении",
+                                      preferredStyle: .alert)
+        alert.addTextField { [weak self] textField in
+            textField.delegate = self
+            textField.text = self?.settingsDataSource.getUserName()
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(UIAlertAction(title: "Сохранить", style: .default) { [weak self] _ in
+            guard let self = self, let newUserName = self.newUserName else { return }
+            self.settingsDataSource.setUserName(newUserName)
+            self.updateData()
+            self.tableView.reloadData()
+        })
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension SettingsController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        newUserName = textField.text
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        newUserName = textField.text
+        return true
+    }
+}
+
 // MARK: - Helper
 private extension SettingsController {
-    func createData() {
+    func setupNavBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editButtonTap))
+    }
+    
+    @objc func editButtonTap() {
+        isUserDetailsEditing.toggle()
+    }
+    
+    func updateData() {
         vms = [
-            SwitcherCellVM(cellName: SettingsCellName(name: .pushNotifications), text: "Push-уведомления", isOn: true),
+            UserDetailsCellVM(cellName: SettingsCellName(name: .userDetails),
+                              avatarData: settingsDataSource.getAvatarData(),
+                              userName: settingsDataSource.getUserName()),
             SpaceCellVM(height: 20),
-            SwitcherCellVM(cellName: SettingsCellName(name: .createNews), text: "Создание новостей", isOn: false)
+            SwitcherCellVM(cellName: SettingsCellName(name: .pushNotifications),
+                           text: "Push-уведомления",
+                           isOn: settingsDataSource.getValue(.pushNotifications)),
+            SpaceCellVM(height: 20),
+            SwitcherCellVM(cellName: SettingsCellName(name: .createNews),
+                           text: "Создание новостей",
+                           isOn: settingsDataSource.getValue(.createNews))
         ]
+    }
+    
+    func showImagePicker(sourceType: UIImagePickerController.SourceType) {
+        imagePicker.delegate = self
+        imagePicker.sourceType = sourceType
+        if case .camera = sourceType {
+            imagePicker.cameraDevice = .front
+        }
+        present(imagePicker, animated: true)
+    }
+    
+    func showRemovingPhotoAlert() {
+        let alert = UIAlertController(title: "Удалить аватар?", message: nil, preferredStyle: .alert)
+        alert.addAction(cancelAction)
+        alert.addAction(UIAlertAction(title: "Удалить", style: .destructive) { [weak self] _ in
+            self?.saveAvatar(Data())
+        })
+        present(alert, animated: true)
+    }
+    
+    func getCroppedImage(sourceImage: UIImage?) -> UIImage? {
+        guard let image = sourceImage else { return nil }
+        let newSize = CGSize(width: 100, height: 100)
+        let newImage = image.resizedImageWithinRect(rectSize: newSize)
+        return newImage.getSquaredImage(sourceImage: newImage)
+    }
+    
+    func saveAvatar(_ data: Data) {
+        settingsDataSource.setValue(data != Data(), for: .avatarAvailable)
+        settingsDataSource.setAvatar(data: data)
+        self.updateData()
+        self.tableView.reloadData()
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension SettingsController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let image = info[.originalImage] as? UIImage, let data = getCroppedImage(sourceImage: image)?.pngData() {
+            saveAvatar(data)
+        }
+        dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
     }
 }
--- InvestInfo/Utils.swift
+++ InvestInfo/Utils.swift
@@ -5,8 +5,58 @@
 //  Created by Albert on 20.09.2022.
 //
 
-import Foundation
+import UIKit
 
 struct Constants {
     static let baseHost = "http://investinfo.freemyip.com"
+}
+
+extension UIImage {
+    /// Returns a image that fills in newSize
+    func resizedImage(newSize: CGSize) -> UIImage {
+        guard self.size != newSize else { return self }
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        
+        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    /// Returns a resized image that fits in rectSize, keeping it's aspect ratio
+    /// Note that the new image size is not rectSize, but within it.
+    func resizedImageWithinRect(rectSize: CGSize) -> UIImage {
+        let widthFactor = size.width / rectSize.width
+        let heightFactor = size.height / rectSize.height
+        let resizeFactor = (size.height > size.width) ? heightFactor : widthFactor
+        let newSize = CGSize(width: size.width/resizeFactor, height: size.height/resizeFactor)
+        return resizedImage(newSize: newSize)
+    }
+    
+    func crop(rect: CGRect) -> UIImage {
+        var rect = rect
+        rect.origin.x*=self.scale
+        rect.origin.y*=self.scale
+        rect.size.width*=self.scale
+        rect.size.height*=self.scale
+        guard let imageRef = self.cgImage?.cropping(to: rect) else { return UIImage() }
+        return UIImage(cgImage: imageRef)
+    }
+    
+    func getSquaredImage(sourceImage: UIImage?) -> UIImage? {
+        guard let image = sourceImage else { return nil }
+        guard image.size.height != image.size.width else { return image }
+        
+        let shortestSide = min(image.size.height, image.size.width)
+        let longestSide = max(image.size.height, image.size.width)
+        
+        let center = longestSide / 2
+        let begin = center - shortestSide / 2
+        
+        let x = image.size.height < image.size.width ? begin : 0
+        let y = image.size.height > image.size.width ? begin : 0
+        
+        return image.crop(rect: CGRect(x: round(x), y: round(y), width: shortestSide, height: shortestSide))
+    }
 }
