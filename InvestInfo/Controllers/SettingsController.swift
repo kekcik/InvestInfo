f2d92913c718c2def4f3ecf98ce0d473d1656634
Vladimir Mikishchenko <v.mikishchenko@gmail.com>
2022-10-07 18:17:28 +0300

    add userFamilyName

--- InvestInfo/Controllers/SettingsController.swift
+++ InvestInfo/Controllers/SettingsController.swift
@@ -11,13 +11,19 @@ final class SettingsController: UITableViewController {
     private var vms: [CommonCellVM] = []
     private var settingsDataSource: SettingsDataProtocol = SettingsDataSouce.shared
     private lazy var addImageService: AddImageServiceProtocol = AddImageService()
-    private var newUserName: String?
-    private var isUserDetailsEditing = false {
-        didSet {
-            updateNavBar()
+    private lazy var templateUserName: UserName = UserName()
+    private enum UserNameTag: Int {
+        case name, familyName
+        var placeholder: String {
+            switch self {
+            case .name:         return "Имя"
+            case .familyName:   return "Фамилия"
+            }
         }
     }
-    
+    private var isUserDetailsEditing = false {
+        didSet { updateNavBar() }
+    }
     private enum CellName: String, Equatable { case noOne, userDetails, pushNotifications, createNews }
     private struct SettingsCellName: CommonCellNameProtocol { var name: CellName = .noOne }
     
@@ -72,15 +78,18 @@ extension SettingsController: UserDetailsEditingProtocol {
     
     func editUserName() {
         guard isUserDetailsEditing else { return }
-        let alert = UIAlertController(title: "Имя пользователя", message: "поможет в общении", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Представьтесь", message: "это поможет в общении", preferredStyle: .alert)
+        let userName = settingsDataSource.getUserName()
         alert.addTextField { [weak self] textField in
-            textField.delegate = self
-            textField.text = self?.settingsDataSource.getUserName()
+            self?.setup(textField: textField, userName: userName, for: (alert.textFields?.count ?? 1) - 1)
+        }
+        alert.addTextField { [weak self] textField in
+            self?.setup(textField: textField, userName: userName, for: (alert.textFields?.count ?? 1) - 1)
         }
         alert.addAction(UIAlertAction(title: "Отменить", style: .cancel))
         alert.addAction(UIAlertAction(title: "Сохранить", style: .default) { [weak self] _ in
-            guard let self = self, let newUserName = self.newUserName else { return }
-            self.settingsDataSource.setUserName(newUserName)
+            guard let self = self else { return }
+            self.settingsDataSource.setUserName(self.templateUserName)
             self.reload()
         })
         present(alert, animated: true)
@@ -90,13 +99,25 @@ extension SettingsController: UserDetailsEditingProtocol {
 // MARK: - UITextFieldDelegate
 extension SettingsController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        newUserName = textField.text
         textField.resignFirstResponder()
         return true
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        newUserName = textField.text
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        update(text: "", for: textField.tag)
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let text = textField.text, let textRange = Range(range, in: text) {
+            let updatedText = text.replacingCharacters(in: textRange, with: string)
+            update(text: updatedText, for: textField.tag)
+        }
         return true
     }
 }
@@ -113,10 +134,11 @@ private extension SettingsController {
     }
     
     func updateData() {
+        let userName = settingsDataSource.getUserName()
         vms = [
             UserDetailsCellVM(cellName: SettingsCellName(name: .userDetails),
                               avatarData: settingsDataSource.getAvatarData(),
-                              userName: settingsDataSource.getUserName()),
+                              userName: [userName.name ?? "", userName.familyName ?? ""].joined(separator: " ")),
             SpaceCellVM(height: 20),
             SwitcherCellVM(cellName: SettingsCellName(name: .pushNotifications),
                            text: "Push-уведомления",
@@ -126,6 +148,30 @@ private extension SettingsController {
                            text: "Создание новостей",
                            isOn: settingsDataSource.getValue(.createNews))
         ]
+    }
+    
+    func setup(textField: UITextField, userName: UserName, for tag: Int) {
+        guard let userNameTag = UserNameTag(rawValue: tag) else { return }
+        textField.delegate = self
+        textField.autocorrectionType = .no
+        textField.keyboardType = .default
+        textField.returnKeyType = .done
+        textField.clearButtonMode = .whileEditing
+        textField.contentVerticalAlignment = .center
+        textField.tag = userNameTag.rawValue
+        textField.placeholder = userNameTag.placeholder
+        switch userNameTag {
+        case .name:         textField.text = userName.name
+        case .familyName:   textField.text = userName.familyName
+        }
+    }
+    
+    func update(text: String, for tag: Int) {
+        guard let userNameTag = UserNameTag(rawValue: tag) else { return }
+        switch userNameTag {
+        case .name:         templateUserName.name = text.isEmpty ? nil : text
+        case .familyName:   templateUserName.familyName = text.isEmpty ? nil : text
+        }
     }
     
     func saveAvatar(_ data: Data?) {
